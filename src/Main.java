1cb5014af28fe8a8baadb8ab4ce6ac6a9e3b4312
schnurluke <schnurle@mail.uc.edu>
2022-09-22 13:19:50 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age =22;
+        if(age >= 21)
+            System.out.println("You get a wristband");
+        else
+            System.out.println("You do not get a wristband");
+    }
+}
